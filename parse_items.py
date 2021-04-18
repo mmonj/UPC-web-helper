85b1492a9a6cfac17f9621fe723a02eef19f62ae
mmonj <mmonj@users.noreply.github.com>
2021-04-18 01:25:45 -0400

    update item parsing

--- parse_items.py
+++ parse_items.py
@@ -1,5 +1,31 @@
 import json
 import re
 
-def main():
+ITEM_DUMP = 'item dump.txt'
+RESULT_JSON = 'items.json'
 
+ITEM_ATTRIBUTES_RE = r'^(.+?) (\d+) (\d) ([a-z]\d+$)$'
+
+def main():
+	with open(ITEM_DUMP, 'r', encoding='utf8') as fd:
+		item_dump = fd.read()
+
+
+	final_dict = {}
+	matches = re.finditer(ITEM_ATTRIBUTES_RE, item_dump, flags=re.MULTILINE|re.IGNORECASE)
+	for match in matches:
+		upc = match.group(2)
+		final_dict[upc] = {
+			'name': match.group(1), 
+			'section': match.group(3), 
+			'location': match.group(4)
+		}
+
+	with open(RESULT_JSON, 'w', encoding='utf8') as fd:
+		json.dump(final_dict, fd, indent=4)
+
+	print('Done!')
+	
+
+if __name__ == '__main__':
+	main()
