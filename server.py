604db4af82c925cd8bc28db754c9472e0b3d5ad6
mmonj <mmonj@users.noreply.github.com>
2021-04-18 01:40:04 -0400

    add upc check function

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+ITEMS_JSON = 'items.json'
 
-@app.route('/')
-def main_page():
-	return 'the main page'
 
 @app.route('/locationcheck')
 def my_route():
-  upc = request.args.get('upc', default = '123', type = str)
-  return upc
+  upc = request.args.get('upc', default = '*', type = str)
+  if upc == '*':
+  	return 'ERROR. No UPC given'
+
+  name, section, location = _get_item_info(upc)
+  if location is None:
+  	return 'Item not present'
+
+  return f'{name}\n\n{section}\n\n{location}'
+
+
+def _get_item_info(upc):
+	with open(ITEMS_JSON, 'r', encoding='utf8') as fd:
+		items = json.load(fd)
+
+	info = items.get(upc)
+	if info is None:
+		return None, None, None
+
+	return info['name'], info['section'], info['location']
