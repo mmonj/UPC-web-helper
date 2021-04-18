9b2ddddfd97707d374c00e887a5427864d80ca70
mmonj <mmonj@users.noreply.github.com>
2021-04-18 15:07:22 -0400

    fix indentation

--- server.py
+++ server.py
@@ -8,28 +8,28 @@ ITEMS_JSON_TEMPLATE = Template('items_T$store_number.json')
 
 @app.route('/T2451-loc-check')
 def my_route1():
-  upc = request.args.get('upc', default=None, type=str)
-  if upc if None:
-    return 'ERROR. No UPC given'
+    upc = request.args.get('upc', default=None, type=str)
+    if upc is None:
+        return 'ERROR. No UPC given'
 
-  info = _get_item_info(upc, '2451')
-  if info is None:
-    return 'Item not present / Discontinued'
+    info = _get_item_info(upc, '2451')
+    if info is None:
+        return 'Item not present / Discontinued'
 
-  return '<br/><br/>'.join(info.values())
+    return '<br/><br/>'.join(info.values())
 
 
 @app.route('/T3277-loc-check')
 def my_route2():
-  upc = request.args.get('upc', default=None, type=str)
-  if upc is None:
-    return 'ERROR. No UPC given'
+    upc = request.args.get('upc', default=None, type=str)
+    if upc is None:
+        return 'ERROR. No UPC given'
 
-  info = _get_item_info(upc, '3277')
-  if info is None:
-    return 'Item not present / Discontinued'
+    info = _get_item_info(upc, '3277')
+    if info is None:
+        return 'Item not present / Discontinued'
 
-  return '<br/><br/>'.join(info.values())
+    return '<br/><br/>'.join(info.values())
 
 
 def _get_item_info(upc, store_number):
