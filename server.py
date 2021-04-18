6b6d1030badee4085cf1d2920ac4ae0fc1990511
mmonj <mmonj@users.noreply.github.com>
2021-04-18 02:34:37 -0400

    implement searching new item files

--- server.py
+++ server.py
@@ -1,29 +1,40 @@
 import json
+from string import Template
 from flask import Flask, request
 
 app = Flask(__name__)
-ITEMS_JSON = 'items.json'
+ITEMS_JSON_TEMPLATE = Template('items_T$store_number.json')
 
 
-@app.route('/locationcheck')
+@app.route('/T2451_loc_check')
 def my_route():
   upc = request.args.get('upc', default = '*', type = str)
   if upc == '*':
-  	return 'ERROR. No UPC given'
+    return 'ERROR. No UPC given'
 
-  name, section, location = _get_item_info(upc)
+  info = _get_item_info(upc, '2451')
+  if info is None:
+    return 'Item not present / Discontinued'
+
+  return '<br/><br/>'.join(info)
+
+
+@app.route('/T3277_loc_check')
+def my_route():
+  upc = request.args.get('upc', default = '*', type = str)
+  if upc == '*':
+    return 'ERROR. No UPC given'
+
+  name, section, location = _get_item_info(upc, '3277')
   if location is None:
-  	return 'Item not present'
+    return 'Item not present'
 
   return f'{name}<br/><br/>{section}<br/><br/>{location}'
 
 
-def _get_item_info(upc):
-	with open(ITEMS_JSON, 'r', encoding='utf8') as fd:
-		items = json.load(fd)
+def _get_item_info(upc, store_number):
+    items_json = ITEMS_JSON_TEMPLATE.safe_substitute(store_number=store_number)
+    with open(items_json, 'r', encoding='utf8') as fd:
+        items = json.load(fd)
 
-	info = items.get(upc)
-	if info is None:
-		return None, None, None
-
-	return info['name'], info['section'], info['location']
+    return items.get(upc)
