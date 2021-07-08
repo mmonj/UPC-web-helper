739f183b383b429f420a04c5c3afaca04db7ace2
mmonj <mmonj@users.noreply.github.com>
2021-07-08 09:09:01 -0400

    improve item parsing

--- parse_items.py
+++ parse_items.py
@@ -1,31 +1,58 @@
+#!python3
+
 import json
+import os
 import re
 
-ITEM_DUMP = 'item dump.txt'
-RESULT_JSON = 'items.json'
+DUMP_FORMAT_RE = r'item dump_(T\d+)\.\w+'
+ITEM_ATTRIBUTES_RE = r'(.+) (\d{12})(?: \d)? ([a-z]\d+)'
 
-ITEM_ATTRIBUTES_RE = r'^(.+?) (\d+) (\d) ([a-z]\d+$)$'
+OUTPUT_FILE = 'items.json'
+
 
 def main():
-	with open(ITEM_DUMP, 'r', encoding='utf8') as fd:
-		item_dump = fd.read()
+    dump_files = (f for f in os.listdir('./') if f.startswith('item dump_'))
+    
+    all_store_items = {}
+    for file in dump_files:
+        store_number = re.search(DUMP_FORMAT_RE, file).group(1)
+
+        items = _parse_items(file)
+        all_store_items[store_number] = {}
+        all_store_items[store_number].update(items)
 
 
-	final_dict = {}
-	matches = re.finditer(ITEM_ATTRIBUTES_RE, item_dump, flags=re.MULTILINE|re.IGNORECASE)
-	for match in matches:
-		upc = match.group(2)
-		final_dict[upc] = {
-			'name': match.group(1), 
-			'section': match.group(3), 
-			'location': match.group(4)
-		}
+    with open(OUTPUT_FILE, 'w', encoding='utf8') as fd:
+        json.dump(all_store_items, fd, indent=4)
 
-	with open(RESULT_JSON, 'w', encoding='utf8') as fd:
-		json.dump(final_dict, fd, indent=4)
 
-	print('Done!')
-	
+
+
+def _parse_items(file):
+    with open(file, 'r', encoding='utf8') as fd:
+        contents = fd.read()
+
+    items = {}
+
+    matches = re.finditer(ITEM_ATTRIBUTES_RE, contents, re.MULTILINE|re.IGNORECASE)
+
+    for match in matches:
+        name = match.group(1)
+        upc = match.group(2)
+        location = match.group(3)
+
+
+        item = {
+            upc: {
+                'name': name,
+                'location': location
+            }
+        }
+
+        items.update(item)
+
+    return items
+
 
 if __name__ == '__main__':
-	main()
+    main()
