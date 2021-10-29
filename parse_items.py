a2b656744a8e43aa0c1eb9cf2dd90821e7a293a1
mmonj <mmonj@users.noreply.github.com>
2021-10-28 23:47:15 -0400

    update regex

--- parse_items.py
+++ parse_items.py
@@ -4,14 +4,15 @@ import json
 import os
 import re
 
-DUMP_FORMAT_RE = r'item dump_(T\d+)\.\w+'
-ITEM_ATTRIBUTES_RE = r'(.+) (\d{12})(?: \d)? ([a-z]\d+)'
+DUMP_FORMAT_RE = r'item dump_(T\d+.*)\.\w+'
+ITEM_ATTRIBUTES_RE = r'(.+?) (\d{12})(?: \d)? ([a-z]\d+)'
 
+DUMP_FOLDER = 'item dumps'
 OUTPUT_FILE = 'items.json'
 
 
 def main():
-    dump_files = (f for f in os.listdir('./') if f.startswith('item dump_'))
+    dump_files = (os.path.join(DUMP_FOLDER, f) for f in os.listdir(DUMP_FOLDER) if f.startswith('item dump_'))
     
     all_store_items = {}
     for file in dump_files:
@@ -20,6 +21,8 @@ def main():
         items = _parse_items(file)
         all_store_items[store_number] = {}
         all_store_items[store_number].update(items)
+
+        print(f'{store_number:<20}:{len(items)} items\n')
 
 
     with open(OUTPUT_FILE, 'w', encoding='utf8') as fd:
@@ -37,7 +40,7 @@ def _parse_items(file):
     matches = re.finditer(ITEM_ATTRIBUTES_RE, contents, re.MULTILINE|re.IGNORECASE)
 
     for match in matches:
-        name = match.group(1)
+        name = match.group(1).strip()
         upc = match.group(2)
         location = match.group(3)
 
