a6a8b764542db0b3b88c1954f539b64b18e1033f
mmonj <mmonj@users.noreply.github.com>
2022-06-29 03:19:12 -0400

    add new

--- upc_logger/upc_logger.py
+++ upc_logger/upc_logger.py
@@ -5,7 +5,6 @@ import time
 from flask import Blueprint, render_template, session, abort, request, Markup
 
 STORE_INFO_FILE = 'static/store_info.json'
-
 app_upc_logger = Blueprint('app_upc_logger', __name__)
 
 @app_upc_logger.route("/upc-log")
@@ -13,7 +12,7 @@ def my_route1():
     font_size = 18
 
     if not os.path.isfile(STORE_INFO_FILE):
-        return render_template('index.html', font_size=font_size, message='Error. No JSON file found.')
+        return render_template('index.html', font_size=font_size, message=f'Error. No JSON file found. "{os.getcwd()}"')
 
     with open(STORE_INFO_FILE, 'r', encoding='utf8') as fd:
         stores = json.load(fd)
