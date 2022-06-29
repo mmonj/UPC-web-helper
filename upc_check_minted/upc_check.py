d9df68eafdc6f1ac08474d0bc0b939a928686d06
mmonj <mmonj@users.noreply.github.com>
2022-06-29 03:42:37 -0400

    add new

--- upc_check_minted/upc_check.py
+++ upc_check_minted/upc_check.py
@@ -18,7 +18,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 ##
 
-ITEMS_JSON = 'items.json'
+ITEMS_JSON = f'{__name__}/items.json'
 app_upc_check = Blueprint('app_upc_check', __name__)
 
 @app_upc_check.route('/loc')
